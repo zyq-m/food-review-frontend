ed868f795d003ae02a34ee4886df51b872fb674c
zyq-m <last.game01987@gmail.com>
2024-05-28 02:05:25 +0800

    create on search restaurant

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,26 @@
 import { Link } from "react-router-dom";
 import useUser from "../hooks/useUser";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../api/api";
+import { useForm } from "react-hook-form";
 
 export default function Header() {
   const { user, setUser } = useUser();
   const url = import.meta.env.VITE_API;
+  const { register, handleSubmit } = useForm();
+  const [restaurant, setRestaurant] = useState([]);
+
+  async function onSearch(data) {
+    try {
+      const res = await api.get("/restaurant/search", {
+        params: data,
+      });
+
+      setRestaurant(res.data.restaurant);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   useEffect(() => {
     !user?.avatar &&
@@ -22,27 +37,34 @@ export default function Header() {
   }, [user?.avatar]);
 
   return (
-    <nav className="flex justify-between my-6">
+    <nav className="flex justify-between items-center my-6">
       <Link to="/" className="text-xl font-extrabold">
         Lapor.com
       </Link>
 
       <div className="flex gap-3">
-        <label className="input input-sm input-bordered flex items-center gap-2 rounded-full">
-          <input type="text" className="grow" placeholder="Search" />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            className="w-4 h-4 opacity-70"
-          >
-            <path
-              fillRule="evenodd"
-              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-              clipRule="evenodd"
+        <form onSubmit={handleSubmit(onSearch)}>
+          <label className="input input-sm input-bordered flex items-center gap-2 rounded-full">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="w-4 h-4 opacity-70"
+            >
+              <path
+                fillRule="evenodd"
+                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <input
+              type="search"
+              className="grow"
+              placeholder="Where to eat?"
+              {...register("restaurant")}
             />
-          </svg>
-        </label>
+          </label>
+        </form>
         <div>
           <Link to="/profile">
             <img
